a0164056339d5621d1649a7becbf28902c6ad434
Madiov <76660455+Madiov@users.noreply.github.com>
2023-08-16 10:32:41 +0330

    Update Program.cs

--- RSSFeeds/Program.cs
+++ RSSFeeds/Program.cs
@@ -50,7 +50,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
